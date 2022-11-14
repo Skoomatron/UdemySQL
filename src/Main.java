06ea1ed3abdbe80f740983b2bc3c5ffc05ac8566
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-11-14 13:11:32 -0500

    Initial DB Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:myTestDB.db");
+        } catch (SQLException e) {
+            System.out.println("Couldn't Connect: " + e.getMessage());
+        }
     }
 }
